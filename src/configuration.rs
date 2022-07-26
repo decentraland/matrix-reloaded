8d9e937d962a1625ec45008db8ffed1847c46a19
guidota <guido@tamborindeguy.com.ar>
2022-07-26 12:43:30 -0300

    fix: use correct domain when url starts with `synapse.`

--- src/configuration.rs
+++ src/configuration.rs
@@ -9,14 +9,19 @@ use std::time::Duration;
 
 /// This function returns homeserver domain and url, ex:
 ///  - get_homeserver_url("matrix.domain.com") => ("matrix.domain.com", "https://matrix.domain.com")
+///  - get_homeserver_url("synapse.domain.com") => ("domain.com", "https://matrix.domain.com")
 pub fn get_homeserver_url(homeserver: &str, default_protocol: Option<&str>) -> (String, String) {
     let regex = Regex::new(r"https?://").unwrap();
     if regex.is_match(homeserver) {
         let parts: Vec<&str> = regex.splitn(homeserver, 2).collect();
-        (parts[1].to_string(), homeserver.to_string())
+        let domain = parts[1].to_string().replace("synapse.", "");
+        (domain, homeserver.to_string())
     } else {
         let protocol = default_protocol.unwrap_or("https");
-        (homeserver.to_string(), format!("{protocol}://{homeserver}"))
+        (
+            homeserver.to_string().replace("synapse.", ""),
+            format!("{protocol}://{homeserver}"),
+        )
     }
 }
 
