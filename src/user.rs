0f339322d91d1ffa557a7602354146a714b92445
lauti7 <lautigbustos@gmail.com>
2022-09-23 13:10:36 -0300

    fix: rollback ratios config

--- src/user.rs
+++ src/user.rs
@@ -583,13 +583,7 @@ fn pick_random_action(
 ) -> SocialAction {
     let mut rng = rand::thread_rng();
     if rng.gen_ratio(probability_to_act as u32, 100) {
-        if rng.gen_ratio(1, 3) {
-            SocialAction::SendMessage(RoomType::DirectMessage)
-        } else if channels_enabled && rng.gen_ratio(1, 5) {
-            SocialAction::SendMessage(RoomType::Channel)
-        } else if rng.gen_ratio(1, 3) {
-            SocialAction::AddFriend
-        } else if rng.gen_ratio(1, 75) {
+        if rng.gen_ratio(1, 75) {
             SocialAction::LogOut
         } else if channels_enabled && rng.gen_ratio(1, 70) {
             SocialAction::LeaveChannel
@@ -601,6 +595,10 @@ fn pick_random_action(
             SocialAction::JoinChannel
         } else if rng.gen_ratio(1, 25) {
             SocialAction::UpdateStatus
+        } else if rng.gen_ratio(1, 3) {
+            SocialAction::AddFriend
+        } else if channels_enabled && rng.gen_ratio(1, 5) {
+            SocialAction::SendMessage(RoomType::Channel)
         } else {
             SocialAction::SendMessage(RoomType::DirectMessage)
         }
